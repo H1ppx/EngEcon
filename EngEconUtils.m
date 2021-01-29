f5642179d415036a6a0b168b1f807ba1d0635672
William Chu <willchu0510@gmail.com>
2021-01-28 20:01:30 -0500

    Initial file

---
+++ EngEconUtils.m
@@ -0,0 +1,20 @@
+classdef EngEconUtils
+    
+    methods(Static)
+
+        % Optimal Demand when Demand is a Function of Price
+        function [sellingPrice, maxProfitDemand, maxProfit, profitRange] = ProfitAnalysis(a_coefficient, b_coefficient, variableCost, fixedCost)
+            maxProfitDemand = (a_coefficient-variableCost)/(2*b_coefficient);
+            sellingPrice = a_coefficient-b_coefficient*maxProfitDemand;
+            maxProfit = -b_coefficient*maxProfitDemand^2+(a_coefficient-variableCost)*maxProfitDemand-fixedCost; 
+            profitRange = sort(roots([(-b_coefficient) (a_coefficient-variableCost) (-fixedCost)]));
+        
+        end
+        
+        % Breakeven Point when Price is Independent of Demand
+        function breakevenPoint = Breakeven(sellingPrice, variableCost, fixedCost)
+            breakevenPoint = fixedCost/(sellingPrice-variableCost);
+        end
+        
+    end
+end
