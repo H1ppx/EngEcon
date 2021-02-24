8ed9ab84e82f68387cb65394d8a58517d8e0873a
Will <willchu0510@gmail.com>
2021-02-24 13:27:50 -0500

    Interest Calculations for Exam 1

--- EngEconUtils.m
+++ EngEconUtils.m
@@ -18,5 +18,57 @@ classdef EngEconUtils
             breakevenPoint = fixedCost/(sellingPrice-variableCost);
         end
         
+        % Price of item taken from historical data with a specified base year and updated withan index.
+        function [estimatedCost] = RatioTechnique(costInReferenceYear, indexAtReferenceYear, indexAtEstimatedYear)
+            estimatedCost = costInReferenceYear * (indexAtReferenceYear/indexAtEstimatedYear);
+        end
+        
+        % Sum the product of several quantities or components and add these to any components estimated directly
+        function [estimatedCost] = FactorTechnique(costofSelectedComponent, unitCost, amountOfUnits)
+            estimatedCost = costofSelectedComponent+(unitCost*amountOfUnits);
+        end
+        
+        % Power Sizing Technique estimating cost
+        function [estimatedCostA] = PowerSizingTechniqueCost(sizeA, sizeB, costB, costCapacityFactor)
+            estimatedCostA = costB*(sizeA/sizeB)^costCapacityFactor;
+        end
+        
+        % Power Sizing Technique estimating capacity factor
+        function [costCapacityFactor] = PowerSizingTechniqueCostCapacityFactor(sizeA, sizeB, costA, costB)
+            costCapacityFactor = log10(costA/costB)/log10(sizeA/sizeB);
+        end
+        
+        function [futureSum] = FGivenP(presentSum, interestRate, periods)
+            futureSum = presentSum*(1+interestRate)^periods;
+        end
+        
+        function [presentSum] = PGivenF(futureSum, interestRate, periods)
+            presentSum = futureSum*(1/(1+interestRate))^periods;
+        end
+       
+        function [interestRate] = IGivenPFN(presentSum, futureSum, periods)
+            interestRate = nthroot(futureSum/presentSum,periods)-1;
+        end
+        
+        function [periods] = NGivenPFI(presentSum, futureSum, interestRate)
+            periods = log10(futureSum/presentSum)/log10(1+interestRate);
+        end
+        
+        function [futureSum] = FGivenA(a, interestRate, periods)
+            futureSum = a*(((1+interestRate)^periods-1)/interestRate);
+        end
+        
+        function [presentSum] = PGivenA(a, interestRate, periods)
+            presentSum = a*(((1+interestRate)^periods-1)/(interestRate*(1+interestRate)^periods));
+        end
+        
+        function [a] = AGivenF(futureSum, interestRate, periods)
+            a = futureSum*(interestRate/(((1+interestRate)^periods)-1));
+        end
+        
+        function [a] = AGivenP(presentSum, interestRate, periods)
+            a = presentSum*((interestRate*((1+interestRate)^periods))/(((1+interestRate)^periods)-1));
+        end
+        
     end
 end
